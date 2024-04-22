3f29550e0876d63ced1b98e738aaf65a005f666b
User <Willspri@gmail.com>
2024-04-23 09:25:30 +1200

    added time function

--- randomNumberMatch/randomNumberMatch.py
+++ randomNumberMatch/randomNumberMatch.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def generateNumber():
     return random.sample(range(1, 41), 6)
@@ -10,6 +11,7 @@ if __name__ == "__main__":
     print("Target Number: ", TargetNumber)
     
     found = False
+    start_time = time.time()
     
     while not found:
         generated = generateNumber()
@@ -20,3 +22,6 @@ if __name__ == "__main__":
         if generatedString == TargetNumber:
             found = True
             print("Match found: ", generatedString)
+            end_time = time.time()
+            timeTaken = end_time - start_time
+            print("Time taken: ", timeTaken, "seconds")
